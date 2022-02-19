864c0b5594ddb1df70e9ae5c13719cb6cdae4d75
sadvik-gowda <sadvikb.d@hcl.com>
2022-02-19 21:09:19 +0530

    addin project

---
+++ src/main/java/com/hcl/user/DemoPrg2Application.java
@@ -0,0 +1,13 @@
+package com.hcl.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPrg2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPrg2Application.class, args);
+	}
+
+}
